e44b02e0a010822ac196faee9a6e703504e5fa93
David Power <david2power@gmail.com>
2025-10-26 14:57:17 +0000

    Start of the project

---
+++ Main.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+import imaplib
+import email
+import os
+
+
+load_dotenv()
+
+username = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
+
+def connect_to_mail():
+    mail=imaplib.IMAP4_SSL("imap.gmail.com")
+    mail.login(username, password)
+    mail.select("inbox")
+    return mail
+
+def search_for_email():
+    mail = connect_to_mail()
+    _, search_data = mail.search(None, '(BODY "unsubscribe")')
+    data = search_data[0].split()
+
+    for num in data:
+        _,data = mail.fetch(num, "(RFC822)")
+        msg = email.message_from_bytes(data[0][1])
+
+        if msg.is_multipart():
+            for part in msg.walk():
+                if part.get_content_type() =="text/html":
+                    html_content = part.get_payload(decode=True).decode()
+                    print(html_content)
+
+        else:
+            content_type = msg.get_content_type()
+            content = msg.get_payload(decode=True).decode()
+
+            if content_type == "text/html":
+                print(content)
+
+
+    mail.logout()
+
+search_for_email()
