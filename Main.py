43cd8650503154858eab492914b6a60ddcb2040f
David Power <david2power@gmail.com>
2025-11-02 15:04:33 +0000

    Added the possibility of extracting the links from html and clicking.

--- Main.py
+++ Main.py
@@ -2,6 +2,9 @@ from dotenv import load_dotenv
 import imaplib
 import email
 import os
+from bs4 import BeautifulSoup
+import requests
+
 
 
 load_dotenv()
@@ -15,10 +18,29 @@ def connect_to_mail():
     mail.select("inbox")
     return mail
 
+
+def extract_links_from_html(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    links = [link["href"] for link in soup.find_all("a", href=True) if "unsubscribe" in link["href"].lower()]
+    return links
+
+def click_link(link):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            print("Successufully visited ", link)
+        else:
+            print("Failed to visit", link, "error code", response.status_code)
+    except Exception as e:
+        print("Error with" , link, str(e))
+
+
 def search_for_email():
     mail = connect_to_mail()
     _, search_data = mail.search(None, '(BODY "unsubscribe")')
     data = search_data[0].split()
+
+    links = []
 
     for num in data:
         _,data = mail.fetch(num, "(RFC822)")
@@ -28,16 +50,27 @@ def search_for_email():
             for part in msg.walk():
                 if part.get_content_type() =="text/html":
                     html_content = part.get_payload(decode=True).decode()
-                    print(html_content)
+                    links.extend(extract_links_from_html(html_content))
 
         else:
             content_type = msg.get_content_type()
             content = msg.get_payload(decode=True).decode()
 
             if content_type == "text/html":
-                print(content)
+                links.extend(extract_links_from_html(content))
 
 
     mail.logout()
+    return links
 
-search_for_email()
+def  save_links(links):
+    with open("links.txt", "w") as f:
+        f.write("\n".join(links))
+
+
+
+links = search_for_email()
+for link in links:
+    click_link(link)
+
+save_links(links)
